5772c390f37a4e538871053d08168b3df0919808
Henning Glatter-Götz <henning@glatter-gotz.com>
2014-03-22 09:36:13 +0100

    Add test case for irregular result key name

--- tests/HGG/Pardot/Test/JsonParserTest.php
+++ tests/HGG/Pardot/Test/JsonParserTest.php
@@ -4,17 +4,38 @@ namespace HGG\Pardot\Test;
 
 use HGG\Pardot\ResponseHandler\JsonResponseHandler;
 
+/**
+ * JsonParserTest
+ *
+ * @author Henning Glatter-Götz <henning@glatter-gotz.com>
+ */
 class JsonParserTest extends \PHPUnit_Framework_TestCase
 {
     /**
+     * Run all sunny day tests
+     *
+     * @param mixed $document
+     * @param mixed $object
+     * @param mixed $expected
+     *
      * @dataProvider allPassDataProvider
+     *
+     * @access public
+     * @return void
      */
     public function testPass($document, $object, $expected)
     {
-        $handler = new JsonResponseHandler($document, $object);
+        $handler = new JsonResponseHandler($object);
+        $handler->parse($document);
         $this->assertEquals($expected, $handler->getResult());
     }
 
+    /**
+     * allPassDataProvider
+     *
+     * @access public
+     * @return void
+     */
     public function allPassDataProvider()
     {
         return array(
@@ -43,6 +64,24 @@ class JsonParserTest extends \PHPUnit_Framework_TestCase
                     )
                 ),
                 'prospect',
+                array(
+                    array('field1' => 'value')
+                )
+            ),
+            array(
+                array(
+                    '@attributes' => array(
+                        'stat' => 'ok',
+                        'version' => 1
+                    ),
+                    'result' => array(
+                        'total_results' => 1,
+                        'visitor_activity' => array(
+                            array('field1' => 'value')
+                        )
+                    )
+                ),
+                'visitorActivity',
                 array(
                     array('field1' => 'value')
                 )
